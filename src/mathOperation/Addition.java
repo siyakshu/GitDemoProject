0df638a3a655478ffd16bbe16c2ee1a17d79dd03
Geniusonali <sonalig9421@gmail.com>
2023-02-22 20:31:01 +0530

    MathOperations

---
+++ src/mathOperation/Addition.java
@@ -0,0 +1,15 @@
+package mathOperation;
+
+public class Addition {
+
+	public static void main(String[] args)
+	{
+		int a=20;
+		int b=30;
+		int result=a+b;
+		System.out.println("Addition="+result);
+		
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sonal
+ *
+ */
+module GitDemoProject {
+}
